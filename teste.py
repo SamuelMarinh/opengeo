c49ea7473f6a128247b3be88d76b6dac22591ed3
SamuelMarinh <samuelmarinhodealmeida@gmail.com>
2024-03-09 23:46:01 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,7 @@
+import where.js
+
+# Imprima a latitude e longitude do Jardim Botânico
+print(where.get_location('Jardim Botânico'))
+
+# Imprima a latitude e longitude do Convento da Penha
+print(where.get_location('Convento da Penha'))
